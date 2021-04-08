175a5751e1a28dd85852ea223c1d514c3ea8fd6a
Aarav Navani <aarav.navani@outlook.com>
2021-04-08 08:46:29 +0530

    Added UsageAttribute

---
+++ Jynx/Attributes/UsageAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jynx.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class UsageAttribute : Attribute
+    {
+        private string usage;
+
+        public UsageAttribute(string usage)
+        {
+            this.usage = usage;
+        }
+
+        public virtual string Usage
+        {
+            get { return usage; }
+        }
+    }
+}
