17592965c9c32e71457521002ce0d13ccbe29b2c
Aarav Navani <aarav.navani@outlook.com>
2021-04-08 10:47:41 +0530

    Added deserialization classes

---
+++ Jynx/Handlers/Deserialized/Rotation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jynx.Handlers.Deserialized
+{
+    public class Current
+    {
+        public int start { get; set; }
+        public int end { get; set; }
+        public string readableDate_start { get; set; }
+        public string readableDate_end { get; set; }
+        public string map { get; set; }
+        public int DurationInSecs { get; set; }
+        public int DurationInMinutes { get; set; }
+        public int remainingSecs { get; set; }
+        public int remainingMins { get; set; }
+        public string remainingTimer { get; set; }
+    }
+
+    public class Next
+    {
+        public int start { get; set; }
+        public int end { get; set; }
+        public string readableDate_start { get; set; }
+        public string readableDate_end { get; set; }
+        public string map { get; set; }
+        public int DurationInSecs { get; set; }
+        public int DurationInMinutes { get; set; }
+    }
+
+    public class Rotation
+    {
+        public Current current { get; set; }
+        public Next next { get; set; }
+    }
+}
---
+++ Jynx/Handlers/Deserialized/Stats.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jynx.Handlers.Deserialized
+{
+    public class Rank
+    {
+        public int rankScore { get; set; }
+        public string rankName { get; set; }
+        public int rankDiv { get; set; }
+        public int ladderPosPlatform { get; set; }
+        public string rankImg { get; set; }
+        public string rankedSeason { get; set; }
+    }
+
+    public class Global
+    {
+        public string name { get; set; }
+        public long uid { get; set; }
+        public string avatar { get; set; }
+        public string platform { get; set; }
+        public int level { get; set; }
+        public Rank rank { get; set; }
+    }
+
+    public class Kills
+    {
+        public string name { get; set; }
+        public int value { get; set; }
+    }
+
+    public class Kd
+    {
+        public int value { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Total
+    {
+        public Kills kills { get; set; }
+        public Kd kd { get; set; }
+    }
+
+    public class Stats
+    {
+        public Global global { get; set; }
+        public Total total { get; set; }
+    }
+}
