0972741bbd2d6df8744aae82e7c71053eb68eed7
Aarav Navani <aarav.navani@outlook.com>
2021-04-08 08:46:36 +0530

    Added Extensions

---
+++ Jynx/Common/Extensions.cs
@@ -0,0 +1,35 @@
+﻿using DSharpPlus.CommandsNext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jynx.Common
+{
+    public static class Extensions
+    {
+        public static async Task<string> ParseCodeBlock(this string s, CommandContext ctx)
+        {
+            if (s == null)
+            {
+                await ctx.Channel.SendMessageAsync("You need to give a code block");
+                return null;
+            }
+
+            var cs1 = s.IndexOf("```lua") + 3;
+            cs1 = s.IndexOf('\n', cs1) + 1;
+            var cs2 = s.LastIndexOf("```");
+
+            if (cs1 == -1 || cs2 == -1)
+            {
+                await ctx.Channel.SendMessageAsync("You need to wrap the code into a code block.");
+                return null;
+            }
+
+            var cs = s.Substring(cs1, cs2 - cs1);
+
+            return cs;
+        }
+    }
+}
