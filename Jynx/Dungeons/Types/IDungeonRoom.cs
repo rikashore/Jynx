36b11cdf45599c61ed8af2ee31c310d26713880c
Aarav Navani <aarav.navani@outlook.com>
2021-04-09 16:46:48 +0530

    Began work on Dungeons

---
+++ Jynx/Dungeons/Types/IDungeonRoom.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jynx.Dungeons.Types
+{
+    public interface IDungeonRoom
+    {
+
+    }
+}
