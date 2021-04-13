53c396ff71e2d382bf822e9a4a1fa0ae7b2fa9be
Aarav Navani <aarav.navani@outlook.com>
2021-04-13 20:10:00 +0530

    Added more extensions

--- Jynx/Common/Extensions.cs
+++ Jynx/Common/Extensions.cs
@@ -40,6 +40,23 @@ namespace Jynx.Common
 
             return cs;
         }
+
+        public static string ToFirstUpper(this string input) =>
+            input switch
+            {
+                null => throw new ArgumentNullException(nameof(input)),
+                "" => throw new ArgumentException($"{nameof(input)} cannot be empty", nameof(input)),
+                _ => input.First().ToString().ToUpper() + input[1..]
+            };
+
+        public static bool In<T>(this T e, params T[] list)
+            => e.In((IEnumerable<T>)list);
+
+        public static bool In<T>(this T e, IEnumerable<T> list)
+            => list.Any(l => (e == null && l == null) || (e?.Equals(l) ?? false));
+
+        public static bool NotIn<T>(this T e, params T[] list)
+            => !In(e, list);
     }
 
     public enum ParseType
