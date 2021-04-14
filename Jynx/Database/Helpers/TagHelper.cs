a31fc912c3244ffb63fe4a5ccd1ef50b2bf9bef4
Aarav Navani <aarav.navani@outlook.com>
2021-04-14 15:28:22 +0530

    Added helpers

---
+++ Jynx/Database/Helpers/TagHelper.cs
@@ -0,0 +1,50 @@
+﻿using Jynx.Database.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jynx.Database.Helpers
+{
+    public class TagHelper
+    {
+        public JynxContext JynxContext { get; set; }
+
+        public TagHelper(JynxContext context)
+        {
+            JynxContext = context;
+        }
+
+        public async Task AddTag(string tagName, string content)
+        {
+            JynxContext.Add(new Tag { Name = tagName, Content = content });
+
+            await JynxContext.SaveChangesAsync();
+        }
+
+        public async Task<Tag> GetTag(string tagName)
+        {
+            var tag = await JynxContext.Tags.FindAsync(tagName);
+
+            if (tag == null)
+                return null;
+
+            return tag;
+        }
+
+        public async Task<int> DeleteTag(string tagName)
+        {
+            var tag = await JynxContext.Tags.FindAsync(tagName);
+
+            if (tag == null)
+                return 0;
+
+            JynxContext.Tags.Remove(tag);
+
+            await JynxContext.SaveChangesAsync();
+
+            return 1;
+        }
+    }
+}
---
+++ Jynx/Database/Helpers/UserHelper.cs
@@ -0,0 +1,69 @@
+﻿using Jynx.Database.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jynx.Database.Helpers
+{
+    public class UserHelper
+    {
+        public JynxContext JynxContext { get; set; }
+
+        public UserHelper(JynxContext context)
+        {
+            JynxContext = context;
+        }
+
+        public async Task IncrementHealthPotions(ulong id)
+        {
+            var user = await JynxContext.Users
+                .FindAsync(id);
+
+            user.HealthPotions++;
+
+            await JynxContext.SaveChangesAsync();
+        }
+
+        public async Task IncrementGold(ulong id, int amount)
+        {
+            var user = await JynxContext.Users
+                .FindAsync(id);
+
+            if (user == null)
+                JynxContext.Add(new User { Id = id, GoldAmount = amount });
+            else
+                user.GoldAmount += amount;
+
+            await JynxContext.SaveChangesAsync();
+        }
+
+        public async Task<int> GetHealthPotions(ulong id)
+        {
+            var user = await JynxContext.Users
+                .FindAsync(id);
+
+            if (user == null)
+            {
+                JynxContext.Add(new User { Id = id, HealthPotions = 2 });
+                await JynxContext.SaveChangesAsync();
+                return 2;
+            }
+
+            else
+                return user.HealthPotions;
+        }
+
+        public async Task<int> GetGold(ulong id)
+        {
+            var user = await JynxContext.Users
+                .FindAsync(id);
+
+            if (user == null)
+                return 0;
+            else
+                return user.GoldAmount;
+        }
+    }
+}
