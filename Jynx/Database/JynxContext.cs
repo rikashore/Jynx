59656d09c09f57115e4425a3183cec777c47d85d
Aarav Navani <aarav.navani@outlook.com>
2021-04-14 15:28:34 +0530

    Added Db context

---
+++ Jynx/Database/JynxContext.cs
@@ -0,0 +1,19 @@
+﻿using Jynx.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jynx.Database
+{
+    public class JynxContext : DbContext
+    {
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public JynxContext(DbContextOptions options) : base(options)
+        { }
+    }
+}
