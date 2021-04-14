c318991055ee783940c94a3d5420b504728eea71
Aarav Navani <aarav.navani@outlook.com>
2021-04-14 15:28:05 +0530

    Added Db Entities

---
+++ Jynx/Database/Entities/Tag.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jynx.Database.Entities
+{
+    public class Tag
+    {
+        [Key]
+        public string Name { get; set; }
+
+        public string Content { get; set; }
+    }
+}
