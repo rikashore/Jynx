d4b5d2d60eebdeee38b76548723b1eb5ebb4df9a
Aarav Navani <aarav.navani@outlook.com>
2021-04-17 17:30:31 +0530

    Added Business Hours attribute

---
+++ Jynx/Attributes/RequireBusinessHoursAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace Jynx.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class RequireBusinessHoursAttribute : CheckBaseAttribute
+    {
+        private static readonly DateTime Now = DateTime.Now;
+        private readonly DateTime _startingWorkHour = new DateTime(Now.Year, Now.Month, Now.Day, 9, 0, 0);
+        private readonly DateTime _endingWorkHour = new DateTime(Now.Year, Now.Month, Now.Day, 20, 0, 0);
+        
+        public override Task<bool> ExecuteCheckAsync(CommandContext ctx, bool help)
+        {
+            return Task.FromResult(Now.Hour > _startingWorkHour.Hour && Now.Hour < _endingWorkHour.Hour);
+        }
+    }
+}
