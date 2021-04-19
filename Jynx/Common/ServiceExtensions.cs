c257f9c3d22d792468b23ed4d1467b166a23f9b6
Aarav Navani <aarav.navani@outlook.com>
2021-04-19 17:28:51 +0530

    Added Service Extensions

---
+++ Jynx/Common/ServiceExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Reflection;
+using Jynx.Common.Attributes;
+using Jynx.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Jynx.Common
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddJynxServices(this IServiceCollection services)
+        {
+            var types = Assembly.GetExecutingAssembly().GetTypes()
+                .Where(x => x.GetCustomAttribute<JynxService>() != null && !x.IsAbstract);
+
+            foreach (var type in types)
+            {
+                services.AddSingleton(type);
+            }
+
+            return services;
+        }
+    }
+}
